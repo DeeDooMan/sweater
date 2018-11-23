11b9bf1437d8ae6c84dbc7660e443b49f742aaa5
DeeDooMan <atai.b.isaev@gmail.com>
2018-11-23 15:24:17 +0600

    Урок №5 Подключаем шаблонизатор Freemarker. (завершено)

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -30,10 +30,20 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model){
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model){
         Iterable<Message> messages = messageRepo.findAll();
 
-        model.put("messages", messages);
+        if (filter !=null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        }
+        //Если пользователь ничего не вводит,
+        //или вводит не корректный запрос то выводится весь список
+        else {
+            messages = messageRepo.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
@@ -53,24 +63,4 @@ public class MainController {
 
         return "main";
     }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model){
-        Iterable<Message> messages;
-
-
-        if (filter !=null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
-        }
-        //Если пользователь ничего не вводит,
-        //или вводит не корректный запрос то выводится весь список
-        else {
-            messages = messageRepo.findAll();
-        }
-
-        model.put("messages", messages);
-
-        return "main";
-    }
-
 }
